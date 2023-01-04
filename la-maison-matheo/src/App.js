a2fa62e618344e0c0396c0ae18a9a944a701ad2b
ZarmaFull <121983671+ZarmaFull@users.noreply.github.com>
2023-01-04 23:50:52 +0100

    a la con
    a la con

---
+++ la-maison-matheo/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p className="iron">
+          {mais_toad()}
+          actually want to git gud but he play blue kayn 
+          
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+  function mais_toad(){
+    return"Sei ";
+  }
+}
+
+export default App;
